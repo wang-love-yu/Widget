9a0d6a44f9bb2d9442969dd87707448985b42af8
lei wang <wanglei1@pcuu.com>
2019-09-30 09:16:06 +0800

    移除不必要东西

--- app/src/main/java/zs/qimai/com/widget/MainActivity.kt
+++ app/src/main/java/zs/qimai/com/widget/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import zs.qimai.com.dialog.WlDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
